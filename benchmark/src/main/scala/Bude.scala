beec7088d8f9e40923703db470b9170ea3cfa454
Tom Lin <tom91136@gmail.com>
2021-01-21 02:46:25 +0000

    experimental support for bude with hipsycl

--- benchmark/src/main/scala/Bude.scala
+++ benchmark/src/main/scala/Bude.scala
@@ -11,17 +11,23 @@ object Bude {
       ctx: Context,
       platform: Platform,
       cmakeOpts: Vector[(String, String)],
+      extraModules: Vector[String],
+      device: String,
       exports: String*
   ) = {
     val repo = ctx.wd / "sycl"
 
     RunSpec(
-      prelude = platform.setup(ctx.platformBinDir) ++ exports.map(e => s"export $e"),
+      prelude =
+        platform.setup(ctx.platformBinDir) ++ extraModules ++ exports.map(e => s"export $e"),
       build = s"cd ${repo.^?} " +:
         cmake(
           target = "bude",
           build = repo / "build",
-          cmakeOpts ++ Vector("CMAKE_C_COMPILER" -> "gcc", "CMAKE_CXX_COMPILER" -> "g++"): _*
+          cmakeOpts ++ Vector(
+            "CMAKE_C_COMPILER"   -> "gcc",
+            "CMAKE_CXX_COMPILER" -> "g++"
+          ): _*
         ) :+
         s"cp ${(repo / "build" / "bude").^?} ${repo.^?}",
       run = Vector(
@@ -29,7 +35,7 @@ object Bude {
         s"${(repo / "bude").^?} -n 65536 -i 8 " +
           s"--deck ${(ctx.wd / "data" / "bm1").^?} " +
           s"--wgsize 0 " +
-          s"""--device "${platform.deviceSubstring}""""
+          s"""--device "$device""""
       )
     )
 
@@ -41,7 +47,7 @@ object Bude {
     name = "bude",
     abbr = "b",
     gitRepo = "https://github.com/UoB-HPC/bude-portability-benchmark.git" -> "master",
-    timeout = 1 minute,
+    timeout = 10 minute,
     extractResult = out => {
       out.linesIterator
         .collect { case s"Largest difference was ${pct}%${_}" => pct.trim.toDoubleOption }
@@ -59,9 +65,9 @@ object Bude {
     run = {
       case (ctx, p, computecpp: Sycl.ComputeCpp) =>
         setup(
-          ctx,
-          p,
-          Vector(
+          ctx = ctx,
+          platform = p,
+          cmakeOpts = Vector(
             "SYCL_RUNTIME"      -> "COMPUTECPP",
             "ComputeCpp_DIR"    -> computecpp.sdk,
             "NUM_TD_PER_THREAD" -> "2",
@@ -72,14 +78,16 @@ object Bude {
               Vector("OpenCL_LIBRARY" -> (computecpp.oclcpu / "x64" / "libOpenCL.so").^)
             case _ => Vector.empty
           }),
+          extraModules = Vector.empty,
+          device = p.deviceSubstring,
           (if (p.isCPU) computecpp.cpuEnvs else computecpp.gpuEnvs): _*
         )
 
       case (ctx, p, dpcpp: Sycl.DPCPP) =>
         setup(
-          ctx,
-          p,
-          Vector(
+          ctx = ctx,
+          platform = p,
+          cmakeOpts = Vector(
             "SYCL_RUNTIME"  -> "DPCPP",
             "DPCPP_BIN"     -> dpcpp.`clang++`,
             "DPCPP_INCLUDE" -> dpcpp.include,
@@ -90,10 +98,33 @@ object Bude {
             }}",
             "NUM_TD_PER_THREAD" -> "2"
           ),
+          extraModules = Vector.empty,
+          device = p.deviceSubstring,
           (if (p.isCPU) dpcpp.cpuEnvs else dpcpp.gpuEnvs): _*
         )
 
-      case (ctx, p, hipsycl: Sycl.hipSYCL) => ???
+      case (ctx, p, hipsycl: Sycl.hipSYCL) =>
+        setup(
+          ctx = ctx,
+          platform = p,
+          cmakeOpts = Vector(
+            "SYCL_RUNTIME"        -> "HIPSYCL",
+            "HIPSYCL_INSTALL_DIR" -> """$(dirname "$(which syclcc)")/..""",
+            "HIPSYCL_PLATFORM"    -> (if (p.isCPU) "cpu" else "gpu"),
+            "DISABLE_ND_RANGE"    -> "ON",
+            "CXX_EXTRA_FLAGS" -> s"-march=${p.march} ${p match {
+              case RomeIsambardMACS | CxlIsambardMACS | V100IsambardMACS =>
+                s"--gcc-toolchain=$EvalGCCPathExpr"
+              case _ => ""
+            }}",
+            "NUM_TD_PER_THREAD" -> "2"
+          ),
+          extraModules = Vector(
+            "module load llvm/10.0",
+            s"module load hipsycl/${hipsycl.commit}/gcc-10.2.0"
+          ),
+          device = "0" // pick the first and presumably only one
+        )
     }
   )
 }
